a9d1ed2be50b25c69a654df199048759e4db8b2b
buithimaixuan <xuanbui2207@gmail.com>
2024-01-03 07:54:43 +0700

    product

---
+++ src/main/java/com/mycompany/testgit/Product.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testgit;
+
+/**
+ *
+ * @author Dell
+ */
+public class Product {
+
+    private String name;
+    private int quantity;
+    private String image;
+
+    public Product() {
+    }
+
+    public Product(String name, int quantity, String image) {
+        this.name = name;
+        this.quantity = quantity;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
